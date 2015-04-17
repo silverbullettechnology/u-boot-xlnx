1c6f4dfb92bd0be211d89da607f97d016a05e709
Dustin McIntire <dustin@silver-bullet-tech.com>
2015-04-17 11:35:13 -0400

    Make boundary 4K per S3MA design
    Signed-off-by: Dustin McIntire <dustin@silver-bullet-tech.com>

--- include/sdhci.h
+++ include/sdhci.h
@@ -221,7 +221,7 @@ struct sdhci_host;
 /*
  * Host SDMA buffer boundary. Valid values from 4K to 512K in powers of 2.
  */
-#define SDHCI_DEFAULT_BOUNDARY_SIZE	(512 * 1024)
+#define SDHCI_DEFAULT_BOUNDARY_SIZE	(4 * 1024)
 #define SDHCI_DEFAULT_BOUNDARY_ARG	(7)
 struct sdhci_ops {
 #ifdef CONFIG_MMC_SDHCI_IO_ACCESSORS
